b866d9f7c6aaaa105ffc4c9b8ffc26d0e15853d6
Alekmz <amalexmuniz@gmail.com>
2023-01-02 21:08:49 -0300

    feat: complete development of the calculator library and tests

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -3,3 +3,25 @@ const { sum } = require("./calculator");
 it("should sum 2 and 2", () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+it("should sum 2 and 2 in string format and the result equals 4", () => {
+  expect(sum("2", "2")).toBe(4);
+});
+
+it("should return an error message on invalid inputs", () => {
+  expect(() => {
+    sum("", 2);
+  }).toThrowError();
+
+  expect(() => {
+    sum([]);
+  }).toThrowError();
+
+  expect(() => {
+    sum({});
+  }).toThrowError();
+
+  expect(() => {
+    sum();
+  }).toThrowError();
+});
