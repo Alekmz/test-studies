5cb81d47e77bec3471547b2a112f68034d10a999
Alekmz <amalexmuniz@gmail.com>
2023-01-08 10:59:08 -0300

    feat: finished the query params library

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require("./queryString");
+const { queryString, parse } = require("./queryString");
 
 describe("Trasform object to query string", () => {
   it("should create a valid query string from object", () => {
@@ -8,5 +8,44 @@ describe("Trasform object to query string", () => {
     };
 
     expect(queryString(obj)).toBe("name=Alex&profession=Frontend");
+  });
+  it("should create a valid query string from object with array values in object property", () => {
+    const obj = {
+      name: "Alex",
+      abilities: ["React", "JS"],
+    };
+
+    expect(queryString(obj)).toBe("name=Alex&abilities=React,JS");
+  });
+  it("should return an error message when the object property value is an object", () => {
+    const obj = {
+      name: "Alex",
+      abilities: {
+        first: "React",
+        second: "TDD",
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
+
+describe("Transform query string to object", () => {
+  it("should convert a query string to object", () => {
+    const Qstring = "name=Alex&profession=Frontend";
+    expect(parse(Qstring)).toEqual({
+      name: "Alex",
+      profession: "Frontend",
+    });
+  });
+
+  it("should convert a query string into an object by transforming values ​​with a comma into arrays", () => {
+    const Qstring = "name=Alex&abilities=React,JS";
+    expect(parse(Qstring)).toEqual({
+      name: "Alex",
+      abilities: ["React", "JS"],
+    });
   });
 });
