e4c4f0d221eb457305fd7d28bd324aeeaa75b58e
Alekmz <amalexmuniz@gmail.com>
2023-01-21 17:02:27 -0300

    feat: getTotal method and relations test

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,8 +1,19 @@
+import find from "lodash/find";
+import remove from "lodash/remove";
+
 export default class Cart {
   items = [];
 
   add(item) {
+    const itemToFindOrRemove = { product: item.product };
+    if (find(this.items, itemToFindOrRemove)) {
+      remove(this.items, itemToFindOrRemove);
+    }
     this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
   }
 
   getTotal() {
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -3,27 +3,63 @@ import Cart from "./Cart";
 describe("Cart test", () => {
   let cart;
 
+  let product = {
+    title: "Nike shoes",
+    price: 45054,
+  };
+
+  let product2 = {
+    title: "Adidas shoes",
+    price: 55060,
+  };
+
   beforeEach(() => {
     cart = new Cart();
   });
+  describe("getTotal()", () => {
+    it("should return 0 when getTotal() is executed in a newly created instance", () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
 
-  it("should return 0 when getTotal() is executed in a newly created instance", () => {
-    const cart = new Cart();
+    it("should multiply the quantity of products by the price and return the total", () => {
+      const itemProduct = {
+        product,
+        quantity: 2, // total 90108
+      };
 
-    expect(cart.getTotal()).toEqual(0);
+      cart.add(itemProduct);
+
+      expect(cart.getTotal()).toEqual(90108);
+    });
+
+    it("should ensure no more than on prodcut exists at a time", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      expect(cart.getTotal()).toEqual(90108);
+    });
+
+    it("should update total when a product gets indluded and then removed", () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(55060);
+    });
   });
 
-  it("should multiply the quantity of products by the price and return the total", () => {
-    const itemProduct = {
-      product: {
-        title: "Nike shoes",
-        price: 45054,
-      },
-      quantity: 2, // total 90108
-    };
-
-    cart.add(itemProduct);
-
-    expect(cart.getTotal()).toEqual(90108);
-  });
+  
 });
